a160777b96f4feb7d5aec444410a4360b9a3fd49
Christian Fei <crifei93@gmail.com>
2019-11-21 20:42:13 +0100

    ava

--- index.test.js
+++ index.test.js
@@ -1,10 +1,10 @@
-const tap = require('tap')
+const test = require('ava')
 const getFreeHttpsProxy = require('.')
 
-tap.test('gets free https proxy', async t => {
+test('gets free https proxy', async t => {
   const [proxy] = await getFreeHttpsProxy()
-  t.true(proxy.host)
+  t.truthy(proxy.host)
   t.true(/\d+\.\d+\.\d+\.\d+/.test(proxy.host))
-  t.true(proxy.port)
+  t.truthy(proxy.port)
   t.true(Number.isFinite(proxy.port))
 })
